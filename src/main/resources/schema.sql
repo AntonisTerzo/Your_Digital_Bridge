4e055d1e3a15b557a47f2910b705c345237fe5e7
AntonisTerzo <antonis.terzopoulos@gmail.com>
2025-01-20 22:30:28 +0100

    added db schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS user
+(
+    id         INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(255)       NOT NULL,
+    last_name  VARCHAR(255)       NOT NULL,
+    adress     VARCHAR(255)       NOT NULL,
+    balance    DECIMAL(10, 2)     NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_credentials
+(
+    id       INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    username VARCHAR(255)       NOT NULL UNIQUE,
+    password VARCHAR(255)       NOT NULL,
+    user_id  INT                NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
+CREATE TABLE IF NOT EXISTS transactions
+(
+    id          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    sender_id   INT                NOT NULL,
+    receiver_id INT                NOT NULL,
+    amount      DECIMAL(10, 2)     NOT NULL,
+    timestamp   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES user (id),
+    FOREIGN KEY (receiver_id) REFERENCES user (id)
+
+);
