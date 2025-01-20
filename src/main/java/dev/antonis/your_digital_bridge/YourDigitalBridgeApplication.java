6f1ebcdd8dd289a145ec5aa31dad2529c0fbc95f
AntonisTerzo <antonis.terzopoulos@gmail.com>
2025-01-20 16:12:35 +0100

    initial commit

---
+++ src/main/java/dev/antonis/your_digital_bridge/YourDigitalBridgeApplication.java
@@ -0,0 +1,13 @@
+package dev.antonis.your_digital_bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourDigitalBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourDigitalBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/antonis/your_digital_bridge/YourDigitalBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.antonis.your_digital_bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourDigitalBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
