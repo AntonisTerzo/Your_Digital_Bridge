1a4e476f182c4cdcaf1ac2cecfddbecd05ddc9ae
AntonisTerzo <antonis.terzopoulos@gmail.com>
2025-01-27 22:51:26 +0100

    huge commit, finished the security and added jwt

--- src/main/java/dev/antonis/your_digital_bridge/security/Security.java
+++ src/main/java/dev/antonis/your_digital_bridge/security/Security.java
@@ -1,7 +1,13 @@
 package dev.antonis.your_digital_bridge.security;
 
+import dev.antonis.your_digital_bridge.security.jwt.AuthTokenFilter;
+import dev.antonis.your_digital_bridge.security.jwt.JwtUtils;
+import dev.antonis.your_digital_bridge.user.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -9,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
@@ -18,8 +25,41 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity
 public class Security {
 
+    private final JwtUtils jtwUtils;
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
+
+    public Security(JwtUtils jtwUtils, UserDetailsServiceImpl userDetailsServiceImpl) {
+        this.jtwUtils = jtwUtils;
+        this.userDetailsServiceImpl = userDetailsServiceImpl;
+    }
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public AuthTokenFilter authJwtTokenFilter() {
+        return new AuthTokenFilter(jtwUtils, userDetailsServiceImpl);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authconfig) throws Exception {
+        return authconfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsServiceImpl);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.
                 csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize ->
@@ -29,14 +69,11 @@ public class Security {
                                 .anyRequest().denyAll())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(withDefaults())
                 .exceptionHandling(exception ->
                         exception.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
-        return http.build();
-    }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        http.authenticationProvider(authenticationProvider());
+        http.addFilterBefore(authJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 }
