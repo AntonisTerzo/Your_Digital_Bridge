91343669e37f14ea1e14b5523ddbc6ea4a0aa459
AntonisTerzo <antonis.terzopoulos@gmail.com>
2025-10-02 21:30:52 +0300

    added redis rate limiting

---
+++ src/main/java/dev/antonis/your_digital_bridge/security/rateLimiting/RedisConfig.java
@@ -0,0 +1,21 @@
+package dev.antonis.your_digital_bridge.security.rateLimiting;
+
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableConfigurationProperties(RateLimitProperties.class)
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setDefaultSerializer(new StringRedisSerializer());
+        return template;
+    }
+}
